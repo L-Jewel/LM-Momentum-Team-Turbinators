699182bdbb7e7e626af8a604e039e59585550575
L-Jewel <ljmito42@gmail.com>
2021-01-27 12:40:21 -0600

    Added mission log

---
+++ mission_log.txt.py
@@ -0,0 +1,86 @@
+Waiting for mavsdk_server to be ready...
+Connected to mavsdk_server!
+Waiting for drone to connect...
+Drone discovered with UUID: 0
+Waiting for drone to have a global position estimate...
+Global position estimate ok
+Fetching amsl altitude at home location....
+Retrieving intial GPS data...
+-- Arming
+Observing in air
+awaiting
+awaiting done
+-- Uploading mission
+awaiting
+Mission progress: 0/2
+awaiting done
+-- Starting mission
+Mission progress: 1/2
+-- Pause and clear mission
+-- Get LiDAR Readings
+-- Retrieving LiDAR Sensor Data
+Compiling LiDAR result dictionary
+-- Making new mission plan
+Change in values:  0.0 1.960533404994898e-05 1.78403594198333
+-- * Analysis Completed  8.7e-06 2.860533404994898e-05 1.78403594198333
+-- * Distance from end point calculated  0.0 0.00040219466595005105 0.00040219466595005105
+-- * Injecting waypoint at 1, Lat: 8.7e-06, Long: 2.860533404994898e-05, Alt1.78403594198333
+-- Uploading updated mission
+-- Resuming mission
+Mission progress: 1/3
+Mission progress: 0/3
+Mission progress: 1/3
+Mission progress: 2/3
+-- Pause and clear mission
+-- Get LiDAR Readings
+-- Retrieving LiDAR Sensor Data
+Compiling LiDAR result dictionary
+-- Making new mission plan
+Change in values:  0.0 3.075350669467174e-05 2.660644747594714
+-- * Analysis Completed  8.7e-06 5.935884074462072e-05 4.444680689578044
+-- * Distance from end point calculated  0.0 0.0003714411592553793 0.0003714411592553793
+-- * Injecting waypoint at 2, Lat: 8.7e-06, Long: 5.935884074462072e-05, Alt4.444680689578044
+-- Uploading updated mission
+-- Resuming mission
+Mission progress: 0/4
+Mission progress: 2/4
+Mission progress: 3/4
+-- Pause and clear mission
+-- Get LiDAR Readings
+-- Retrieving LiDAR Sensor Data
+Compiling LiDAR result dictionary
+-- Making new mission plan
+Change in values:  0.0 4.596862262806195e-05 3.587269425099326
+-- * Analysis Completed  8.7e-06 0.00010532746337268268 8.03195011467737
+-- * Distance from end point calculated  0.0 0.00032547253662731733 0.00032547253662731733
+-- * Injecting waypoint at 3, Lat: 8.7e-06, Long: 0.00010532746337268268, Alt8.03195011467737
+-- Uploading updated mission
+-- Resuming mission
+Mission progress: 3/5
+Mission progress: 0/5
+Mission progress: 3/5
+Mission progress: 4/5
+-- Pause and clear mission
+-- Get LiDAR Readings
+-- Retrieving LiDAR Sensor Data
+Compiling LiDAR result dictionary
+-- Making new mission plan
+Change in values:  nan inf -inf
+-- * Analysis Completed  nan inf -inf
+-- * Distance from end point calculated  nan inf nan
+-- * Injecting waypoint at 4, Lat: nan, Long: inf, Alt-inf
+-- Uploading updated mission
+-- Resuming mission
+Mission progress: 0/6
+Mission progress: 4/6
+Mission progress: 5/6
+-- Pause and clear mission
+-- Get LiDAR Readings
+-- Retrieving LiDAR Sensor Data
+Compiling LiDAR result dictionary
+-- Making new mission plan
+Change in values:  nan inf -inf
+-- * Analysis Completed  nan inf -inf
+-- * Distance from end point calculated  nan inf nan
+-- * Injecting waypoint at 5, Lat: nan, Long: inf, Alt-inf
+-- Uploading updated mission
